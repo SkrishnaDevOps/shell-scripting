a3b11b054f1e33c5f3fba34faa003db31586a13b
sai <saikrishna99.t@gmail.com>
2021-12-31 16:13:01 +0530

    Update Shell-Scripting/

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
+aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
